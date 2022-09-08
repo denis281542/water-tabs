e2562140e53ce4179c604a1025c2fed21b91c69d
Denis Kuimov <deniskuimov@mail.ru>
2022-09-08 19:43:15 +0300

    edit input component as presentation component

--- src/components/Input.jsx
+++ src/components/Input.jsx
@@ -1,29 +1,65 @@
 import { useState } from "react";
 
-const Input = ({htmlFor, label, type, id, name, errorMessage, validateInput}) => {
+const Input = ({htmlFor, label, type, id, name, isValid, errorMsg, onChange, value}) => {
     const [error, setError] = useState(false);
-    const [value, setValue] = useState('');
-    // const [errorMessage, setErrorMessage] = useState('');
+    const [errorsPassword, setErrorsPassword] = useState([]);
+    const [errorMessage, setErrorMessage] = useState(errorMsg);
 
-    const onBlur = () => {
-        console.log(validateInput(value));
-        validateInput(value) ? setError(false) : setError(true)
-        // if(!value.length) {
-        //     setError(true)
-        //     setErrorMessage(`Заполните ${label.toLowerCase()}`)
-        // } else {
-        //     console.log(validateInput(value));
-        //     if(!validateInput(value)) {
-        //         setError(true) 
-        //         setErrorMessage(`Поле ${label.toLowerCase()} указано неверно`)
-        //     } else {
-        //         setError(false) 
-        //         setErrorMessage(``)
-        //     }
-        // }
+    const checkInput = (validation, value, errorMsg) => {
+        new Promise((resolve) => {
+            console.log(value);
+            if(!validation(value)) {
+                if(value.length === 0) { 
+                   setErrorMessage('Заполните поле');
+                   setError(true);
+                } else {
+                    setErrorMessage('');
+                    resolve(setError(false))                 
+                } 
+           }
+        }).then(() => {
+            setError(true);
+            setErrorMessage(errorMsg);
+        })        
     }
 
-    // console.log(!!errorMessage);
+    const checkPassword = (value) => {      
+        if(/[a-zA-Z]/gm.test(value)) {
+            new Promise((resolve) => {
+                    if(value.length < 8) {
+                        setErrorMessage('Пароль должен быть не короче 8 символов');
+                        setError(true);
+                    } else if(value.length >= 8 && value.length <= 14) {
+                        setErrorMessage('');
+                        resolve(setError(false));
+                    } else {
+                        setErrorMessage('Пароль должен быть не длинее 14 символов');
+                        setError(true);
+                    }
+            }).then(() => {
+                !/\d/.test(value) && setErrorsPassword(errorsPassword.push('цифры'))
+                !/[A-Z]/.test(value) && setErrorsPassword(errorsPassword.push('заглавные буквы'))
+                !/[a-z]/.test(value) && setErrorsPassword(errorsPassword.push('строчные буквы'))
+                
+                if(errorsPassword.length > 1) {
+                    setErrorMessage(`Пароль должен содержать ${errorsPassword.splice(',').join(' и ')}`)
+                    setError(true)
+                } else if(errorsPassword.length === 1)  {
+                    setErrorMessage(`Пароль должен содержать ${errorsPassword}`)
+                    setError(true)
+                } else {
+                    setErrorMessage(``)
+                    setError(false)
+                }
+            }) 
+        } else if(value.length === 0) {
+            setError(true);
+            setErrorMessage('Заполните пароль');
+        } else {
+            setError(true);
+            setErrorMessage('Пароль должен быть на английском');
+        }
+    }
     
     return(
     <div className="login__field">
@@ -35,11 +71,14 @@ const Input = ({htmlFor, label, type, id, name, errorMessage, validateInput}) =>
             className={!error ? "login__input" : "login__input login__input--error"}
             type={type}
             id={id}
-            name={name}
-            onChange={e => setValue(e.target.value)}           
+            name={name}          
+            onChange={onChange}           
             value={value}
-            onFocus={() => setError(false)}
-            onBlur={onBlur}
+            onFocus={() => {
+                setError(false)
+                setErrorsPassword([])
+            }}
+            onBlur={() => name === 'password' ? checkPassword(value) : checkInput(isValid, value, errorMsg)}
         />
         {error && <small className="error">{errorMessage}</small>}
     </div>)
--- src/components/RegistrationFrom.jsx
+++ src/components/RegistrationFrom.jsx
@@ -5,120 +5,48 @@ import { registration } from "../store/user/userSlice";
 import Button from "./Button";
 import Input from "./Input";
 
+const initialValues = {
+    name: "",
+    login: "",
+    email: "",
+    password: "",
+    passwordConfirm: "",
+  };
+
 const RegistrationFrom = () => {
-    const [name, setName] = useState('');
-    const [login, setLogin] = useState('');
-    const [email, setEmail] = useState('');
-    const [password, setPassword] = useState('');
-    const [passwordConfirm, setPasswordConfirm] = useState('');
-
-    const [nameError, setNameError] = useState(() => {
-        return {error: false, message: null}
-    });
-    const [loginError, setLoginError] = useState(() => {
-        return {error: false, message: null}
-    });
-    const [emailError, setEmailError] = useState(() => {
-        return {error: false, message: null}
-    });
-    const [passwordError, setPasswordError] = useState(false);
-    const [passwordConfirmError, setPasswordConfirmError] = useState(false);
-
-    const [errorMessage, setErrorMessage] = useState('');
-
     const dispatch = useDispatch();
-    const isLoginExist = useSelector(state => state.user.isLoginExist);
+
+
 
     const openLoginModal = () => {
         dispatch(switchLogin(true))
-    }
+    }    
 
-    const isInputEmpty = (value, setValue) => {
-        !value.length ? setValue({error: true, message: "Поле не должно быть пустым"}) : setValue({error: false})
-    }
-
-    const checkInputs = () => {
-        isInputEmpty(name, setNameError)
-        isInputEmpty(login, setLoginError)
-        isInputEmpty(email, setEmailError)
-        isInputEmpty(password, setPasswordError)
-    }
-
-    const comparePasswords = () => {
-        if(password !== passwordConfirm) {
-            setPasswordConfirmError(true)
-            return false
-        } else return true
-    }
-
-    const isEmail = (email) => {
-        const isEmail =  String(email).toLowerCase().match(
-            /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
-        );
-        return isEmail && true
-    }; 
-
-
-    /////////////
+    const validationName = value => /[а-яА-ЯЁё]/.test(value);
+    const validationLogin = login => /[a-zA-Z]/gm.test(login);
+    const validationEmail = email => String(email).toLowerCase().match(
+        /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+    );
     
-    // const isCyrillic = (word, ) => {
-    //     const isValid = /[а-яА-ЯЁё]/.test(word)
         
-    //     if(!isValid) {
-    //          if(word.length === 0) { 
-    //             setErrorMessage('Заполните поле') 
-    //         } else setErrorMessage('Имя должно быть на русском') 
-    //     } else setErrorMessage('') 
-    // }
-
-
-    const validation = value => /[а-яА-ЯЁё]/.test(value)
-
-    const checkInput = (validation, value, errorValidate) => {       
-        if(!validation(value)) {
-             if(value.length === 0) { 
-                setErrorMessage('Заполните поле') 
-            } else setErrorMessage(errorValidate) 
-        } else setErrorMessage('') 
-    }
-
-    const isLogin = login => {
-        const isValid = /[a-zA-Z]/gm.test(login)
-        return isValid && true
-    } 
-
-    const isPassword = password => {
-        const isValid = /[a-zA-Z]/gm.test(password)
-
-        if(password.length === 0) {
-            setErrorMessage('Заполните пароль')
-            return 
-        }
-
-        if(!isValid) {
-            setErrorMessage('Пароль должен быть на английском')
-            return 
-        }
-        
-        password.length < 6 || password.length > 14 
-            ? setErrorMessage('Пароль должен быть от 6 до 14 символов') 
-            : setErrorMessage('')
-    }
-
-    //исправить ошибку валидации пароля при первой ошибке
-    
-    useEffect(() => {
-        isLoginExist ? setLoginError(true) : setLoginError(false);
-    }, [isLoginExist])
 
     const submit = () => {
-        checkInputs();
-        comparePasswords(); 
-        isLogin(login) 
-        isEmail(email) 
 
-        // dispatch(registration({ name, login, email, password }))
     }
+
+
+    const [values, setValues] = useState(initialValues);
+
+
+    const handleInputChange = (e) => {
+        const { name, value } = e.target;
+        setValues({
+          ...values,
+          [name]: value,
+        });
+    };
+
+      console.log(values);
     
   return (<>
     <div className="login__fields">
@@ -128,8 +56,10 @@ const RegistrationFrom = () => {
             type="text"
             id="name"
             name="name"
-            validateInput={name => checkInput(validation, name, 'Имя должно быть на русском')}
-            errorMessage={errorMessage}
+            isValid={validationName}
+            errorMsg='Имя должно быть на русском'            
+            value={values.name}
+            onChange={handleInputChange}
         />
         <Input
             htmlFor="login"
@@ -137,49 +67,31 @@ const RegistrationFrom = () => {
             type="text"
             id="login"
             name="login"
-            validateInput={login => isLogin(login)}            
-            // onChange={e => setLogin(e.target.value)}
-            // value={login}
-            // error={loginError.error}                
-            // onFocus={() => setLoginError({error: false})}
-            // onBlur={() => {
-            //     !login.length 
-            //         ? setLoginError({error: true, message: "Заполните логин"}) 
-            //         : isLogin(login)
-            // }}
-            errorMessage={isLoginExist ? 'Пользователь с таким логином уже существует' : loginError.message}
+            isValid={validationLogin}
+            errorMsg='Логин должен быть на английском'            
+            value={values.login}
+            onChange={handleInputChange}
         />
         <Input
             htmlFor="email"
             label="Email"
             type="email"
             id="email"
-            name="email"
-            validateInput={email => isEmail(email)}            
-            // onChange={e => setEmail(e.target.value)}
-            // value={email}
-            // error={emailError.error}                
-            // onFocus={() => setEmailError({error: false})}
-            // onBlur={() => {
-            //     !email.length 
-            //         ? setEmailError({error: true, message: "Заполните email"}) 
-            //         : isEmail(email)
-            // }}
-            errorMessage={emailError.message} 
+            name="email"            
+            isValid={validationEmail}
+            errorMsg='Email введен неверно'            
+            value={values.email}
+            onChange={handleInputChange}
         />
         <Input
             htmlFor="password"
             label="Пароль"
             type="text"
             id="password"
-            name="password"
-            validateInput={password => isPassword(password)} 
-            errorMessage={errorMessage}          
-            // onChange={e => setPassword(e.target.value)}
-            // value={password}
-            // error={passwordError.error}                
-            // onFocus={() => setPasswordError({error: false})}
-            // onBlur={() => !password.length && setPasswordError({error: true, message: "Заполните пароль"})}
+            name="password" 
+            isValid={() => ''}          
+            value={values.password}
+            onChange={handleInputChange}
         />
         <Input
             htmlFor="passwordConfirm"
@@ -187,11 +99,18 @@ const RegistrationFrom = () => {
             type="text"
             id="passwordConfirm"
             name="passwordConfirm"
-            // onChange={e => setPasswordConfirm(e.target.value)}
-            // value={passwordConfirm}
-            // error={passwordConfirmError}                
-            // onFocus={() => setPasswordConfirmError(false)}
-            // errorMessage="Пароли не совпадают"
+            isValid={() => false}
+
+
+
+            //доделать в др инпуты
+            value={values.passwordConfirm}
+            onChange={handleInputChange}
+
+
+
+
+
         />
     </div>
     
